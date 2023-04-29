ebe54f07904ab112d3eff0fc083c3006f97874b8
Halil Umut Yalçın <umutyalcin3737@gmail.com>
2023-04-29 17:30:39 +0300

    29.04.2023 17.30
    temel durumda

---
+++ Rent-A-Car/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute("/Anasayfa","{controller=Anasayfa}/{action=Index}/{id?}");
+
+app.Run();
+// Araçların plaka, marka, model ve renk bilgisi tutulmalıdır. Her bir aracın dahil olduğu sınıfa 
+// ilişkin ID, sınıf ve kiralama bedeli bilgisi tutulmalıdır. Bir müşteri aynı anda birçok araç 
+// kiralayabilir. Müşterilerin TCK numarası, adı, adresi ve telefon numarası tutulmalıdır. Her bir 
+// kiralama işlemine ilişkin ise tarih ve ücret bilgisi tutulmalıdır.
