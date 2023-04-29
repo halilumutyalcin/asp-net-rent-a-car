36853a192027757ded9bbf6f8fa488d40240272c
Halil Umut Yalçın <umutyalcin3737@gmail.com>
2023-04-29 17:10:20 +0300

    Site ayağa kaldırıldı 29.04.2023 17.10

---
+++ Rent-A-Car/Controllers/Anasayfa.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rent_A_Car.Controllers;
+
+public class Anasayfa : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
