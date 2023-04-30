86af9f8e0567e78cb05663d3f628ad53c1ace8de
Halil Umut Yalçın <umutyalcin3737@gmail.com>
2023-04-30 21:27:38 +0300

    create edit delete eklendi

--- Rent-A-Car/Controllers/CarsController.cs
+++ Rent-A-Car/Controllers/CarsController.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rent_A_Car.Models;
 using System.Data.SqlClient;
+using System.Reflection;
 
 namespace Rent_A_Car.Controllers
 {
     public class CarsController : Controller
     {
-        public Araba araba = new Araba();
-
 
         [HttpGet]
         public IActionResult Create()
@@ -18,7 +17,7 @@ namespace Rent_A_Car.Controllers
 
 
         [HttpPost]
-        public IActionResult Create(Araba model) //int UrunID,string UrunAdi,string UrunMarka, string UrunKategori,int StokAdedi
+        public IActionResult Create(Araba model) 
         {
 
             try
@@ -47,10 +46,128 @@ namespace Rent_A_Car.Controllers
             {
                 Console.WriteLine(ex.Message);
             }
-            return View();
+            return RedirectToAction("Index", "Anasayfa");
 
         }
 
+        [HttpGet]
+
+        public IActionResult Edit(int id) 
+        {
+            Araba araba = new Araba();
+            
+            try
+            {
+                String connectionString = "Data Source=ASUS;Initial Catalog=cars;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    connection.Open();
+                    String sql = "Select * from arabalarTablosu Where id="+id.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        //command.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = command.ExecuteReader()) {
+
+                            if (reader.Read())
+                            {
+
+
+                                araba.ArabaID = reader.GetInt32(0);
+                                araba.ArabaPlaka = reader.GetString(1);
+                                araba.ArabaMarka = reader.GetString(2);
+                                araba.ArabaModel = reader.GetString(3);
+                                araba.ArabaRenk = reader.GetString(4);
+
+                            }
+                           
+                                        
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            ViewBag.id = araba.ArabaID;
+            ViewBag.plaka = araba.ArabaPlaka;
+            ViewBag.marka = araba.ArabaMarka;
+            ViewBag.model = araba.ArabaModel;
+            ViewBag.renk = araba.ArabaRenk;
+            return View();
+        }
+
+
+
+        [HttpPost]
+
+        public IActionResult Edit(Araba model)
+        {
+            Console.WriteLine(model);
+
+            try
+            {
+                String connectionString = "Data Source=ASUS;Initial Catalog=cars;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    connection.Open();
+                    String sql = "UPDATE arabalarTablosu SET plaka=@plaka, marka=@marka, model=@model, renk=@renk WHERE id=@id";
+
+                    Console.WriteLine(sql);
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", model.ArabaID);
+                        command.Parameters.AddWithValue("@plaka", model.ArabaPlaka);
+                        command.Parameters.AddWithValue("@marka", model.ArabaMarka);
+                        command.Parameters.AddWithValue("@model", model.ArabaModel);
+                        command.Parameters.AddWithValue("@renk", model.ArabaRenk);
+                        command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return RedirectToAction("Index", "Anasayfa");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                String connectionString = "Data Source=ASUS;Initial Catalog=cars;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    connection.Open();
+                    String sql = "Delete from arabalarTablosu Where id=" + id.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        //command.Parameters.AddWithValue("@id", id);
+                        command.ExecuteNonQuery();
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return RedirectToAction("Index", "Anasayfa");
+        }
 
 
     }
